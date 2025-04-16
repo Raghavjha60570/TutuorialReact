3f87898c209bedd5203845e1305b7557ab699cd3
Melisandre_64 <Rjha60571@gmail.com>
2025-04-16 19:36:54 +0530

    Created My first Components

---
+++ React/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <footer>
+        <p>© 2023 My Company</p>
+    </footer>
+  );
+}                       
+
+export default Footer;
---
+++ React/src/Header.jsx
@@ -0,0 +1,20 @@
+ 
+
+function Header(){
+     
+    return(
+        <header>
+            <h2>Welcome to the Home Page </h2>
+            <nav>
+                <ul>
+                    <li><a href="#"> Home</a></li>
+                    <li><a href="#">`About </a></li>
+                    <li><a href="#">Service</a></li>
+                    <li><a href="#"> Contac Us </a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+ }
+ export default Header;
+ 
